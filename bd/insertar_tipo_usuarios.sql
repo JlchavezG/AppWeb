7a93ae142abc4ca4fbbaf3cdebbf00727285b019
IscJlchavezG <46545953+JlchavezG@users.noreply.github.com>
2024-09-30 22:15:28 -0600

    Creacion e integracion de bd appweb

---
+++ bd/insertar_tipo_usuarios.sql
@@ -0,0 +1,4 @@
+INSERT INTO AppWeb.TipoUsuario(Id_Tusuario, NomTuser, DescTuser)VALUES(NULL,'Super Usuario','persona responsable de la administración y configuración de todo el sistema, y es el único que tiene permisos para añadir nuevos usuarios'),
+(NULL,'Paciente','observaciones y comentarios: Incluye una descripción sobre las características físicas y psicológicas del evaluado'),
+(NULL,'Especialista','se dedica al estudio, investigación y comprensión de la estructura psíquica de los individuos, estableciendo el psicodiagnóstico y el tratamiento'),
+(NULL,'Invitado','Usuario con version de prueba a la plataforma');
